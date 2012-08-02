c8ce38eb70430a823a7a6d20d5a13e84ad8111ac
Alex Oberhauser <alex.oberhauser@sigimera.org>
2012-08-02 20:52:52 +0200

    Updating the sinatra app.

--- sinatra/client.rb
+++ sinatra/client.rb
@@ -1,9 +1,12 @@
 require 'rubygems'
 require 'sinatra/base'
 
+require 'json'
+require 'open-uri'
+
 class SigimeraApiClient < Sinatra::Base
-    configure :production do
-        # TODO: Add here configuration entries if needed
+    configure do
+        set :auth_token, ENV['AUTH_TOKEN']
     end
 
     set :public_folder, File.dirname(__FILE__) + '/static'
@@ -11,4 +14,31 @@ class SigimeraApiClient < Sinatra::Base
     get '/' do
         erb :index
     end
+
+    get '/red_alerts' do
+        @crises = JSON(get_crises(1, nil, 'red'))
+        erb :red_alerts
+    end
+
+    private
+    def get_crises(page = 1, type = nil, alert_level = nil)
+        url = "http://www.sigimera.org/api/v1/crises?auth_token=#{settings.auth_token}&page=#{page}"
+        unless type.nil?
+            url += "&type=#{type}"
+        end
+
+        unless type.nil?
+            url += "&type=#{type}"
+        end
+
+        unless alert_level.nil?
+            url += "&level=#{alert_level}"
+        end
+
+        crises_endpoint = open(url)
+        crises = crises_endpoint.read
+        crises_endpoint.close
+        crises
+    end
+
 end
